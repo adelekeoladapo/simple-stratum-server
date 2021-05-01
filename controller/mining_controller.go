87a0346748ca446e4dda89f693b86993f3548d3c
Dapo Adeleke <adelekeoladapo@gmail.com>
2021-05-01 12:54:10 +0100

    Created mining controller methods to handle requests

---
+++ controller/mining_controller.go
@@ -0,0 +1,15 @@
+package controller
+
+type Mining int
+
+func (mining *Mining) Authorize() (e error) {
+	return
+}
+
+func (mining *Mining) Subscribe() (e error) {
+	return
+}
+
+func (mining *Mining) Notify() (e error) {
+	return
+}
