2298c685cc2f242ee4d92a0ddc46b681bdfb2c5f
Dapo Adeleke <adelekeoladapo@gmail.com>
2021-05-01 13:27:24 +0100

    Imported Gorm to manage db connection, query and migration

---
+++ db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"log"
+	"luxot.tech/stratum/model"
+)
+
+var Db *gorm.DB
+
+func InitPostgresDB(dbUser string, dbPwd string, dbName string, dbHost string, dbPort string) {
+	var err error
+	dbUrl := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbPort, dbUser, dbName, dbPwd)
+	Db, err = gorm.Open("postgres", dbUrl)
+	if err != nil {
+		fmt.Printf("Failed to connect to database %v", err)
+		log.Panic("Database connection failed")
+	} else {
+		fmt.Println("Database connection successful")
+		runDatabaseMigration()
+	}
+}
+
+func runDatabaseMigration() {
+	Db.AutoMigrate(&model.AuthorizationRequest{}, &model.Subscription{})
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module luxot.tech/stratum
 
 go 1.14
+
+require github.com/jinzhu/gorm v1.9.16 // indirect
