7bca208e01d3e99b24d53baf1ef5fad511a08cb1
Dapo Adeleke <adelekeoladapo@gmail.com>
2021-05-01 14:30:11 +0100

    Created a client to test Mining.Subscribe

--- client.go
+++ client.go
@@ -16,12 +16,19 @@ func main() {
 	defer connection.Close()
 	client := jsonrpc.NewClient(connection)
 
+	/* Test Mining.Authorize */
 	var authorizationResponse bool
 	if err := client.Call("Mining.Authorize", dto.AuthorizeRequest{"dapoadeleke", "password123"}, &authorizationResponse); err != nil {
 		log.Fatalf("An error occurred, %s", err)
 	}
-
-	fmt.Printf("Authorization response: %v", authorizationResponse)
+	fmt.Printf("Mining.Authorize response: %v", authorizationResponse)
 	fmt.Println()
+
+	/*Test Mining.Subscribe */
+	var subscribeResponse dto.SubscribeResponse
+	if err := client.Call("Mining.Subscribe", "", &subscribeResponse); err != nil {
+		log.Fatalf("An error occurred: %s", err)
+	}
+	fmt.Println("Mining.Subscribe response: ", subscribeResponse)
 
 }
